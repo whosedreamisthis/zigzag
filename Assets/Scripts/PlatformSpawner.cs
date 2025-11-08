a81f24bd021cdc9de7314849b6d674d2518bc542
Dana Sharon <whosedreamisthis@gmail.com>
2025-11-07 19:47:55 -0800

    randomly spawn diamonds

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PlatformSpawner : MonoBehaviour
 {
     public GameObject platform;
+    public GameObject diamond;
     Vector3 lastPos;
     float size;
 
@@ -12,7 +13,7 @@ public class PlatformSpawner : MonoBehaviour
     {
         lastPos = platform.transform.position;
         size = platform.transform.localScale.x;
-
+       
         SpawnPlatforms();   
         // SpawnPlatforms();
         InvokeRepeating("SpawnPlatform", 2f,0.2f);
@@ -56,14 +57,28 @@ public class PlatformSpawner : MonoBehaviour
         GameObject p = Instantiate(platform, pos, Quaternion.identity);
         p.transform.parent = transform;
 
+        SpawnDiamond(pos);
+
     }
-  
-  void SpawnZ()
-  {
-     Vector3 pos = lastPos;
+
+    void SpawnZ()
+    {
+        Vector3 pos = lastPos;
         pos.z += size;
         lastPos = pos;
         GameObject p = Instantiate(platform, pos, Quaternion.identity);
         p.transform.parent = transform;
+        SpawnDiamond(pos);
+    }
+  
+    void SpawnDiamond(Vector3 pos)
+    {
+        pos.y += 1;
+        float randVal = Random.value;
+
+        if (randVal < 0.25f)
+        {
+            Instantiate(diamond, pos, diamond.transform.rotation);
+        }
   }
 }

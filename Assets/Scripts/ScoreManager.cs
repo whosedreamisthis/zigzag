f1d9caac620fea67c65be80d7ce0e959c22047e4
Dana Sharon <whosedreamisthis@gmail.com>
2025-11-08 15:19:20 -0800

    score manager

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public int score;
+    public int highScore;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        score = 0;
+        PlayerPrefs.SetInt("score", score);
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    void IncrementScore()
+    {
+        score += 1;
+    }
+
+    public void StartScore()
+    {
+        InvokeRepeating("IncrementScore", 0.1f, 0.5f);
+    }
+
+    public void StopScore()
+    {
+        CancelInvoke("IncrementScore");
+        PlayerPrefs.SetInt("score", score);
+
+        if (PlayerPrefs.HasKey("highScore"))
+        {
+            if (score > PlayerPrefs.GetInt("highScore"))
+            {
+                PlayerPrefs.SetInt("highScore", score);
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("highScore", score);
+        }
+    }
+}
